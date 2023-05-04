19e731d7adaaa4613f7b410cd399d35e56f496dd
Jihye Lee <102567820+jihyeleekr@users.noreply.github.com>
2023-05-04 13:37:38 -0400

    Greetings
    System.out.println(); practice codes

---
+++ Greeting/src/Greeting/Greeting.java
@@ -0,0 +1,15 @@
+package Greeting;
+
+public class Greeting {
+	// Jihye Lee
+	// 1/23/2023
+	// Assignment 1. Greetings
+	// This program prints a greeting message.
+	public static void main(String[] args) {
+		System.out.println("Good morning, Bin!");
+		System.out.println("Nice to see you!");
+		System.out.println("I hope you have a wonderful day!");
+	}
+
+}
+
